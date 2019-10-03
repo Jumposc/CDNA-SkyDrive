dfe96bbf27ddf3eae601c2477e4b421566f688ac
285760010 <285760010@qq.com>
2019-10-03 17:22:08 +0800

    day3

---
+++ CDNA-SkyDrive/wwwroot/js/login.js
@@ -0,0 +1,6 @@
+﻿function login() {
+    var LoginInfo = { "username": "", "password": "" };
+    LoginInfo.username = document.getElementById("input-user").value;
+    LoginInfo.password = document.getElementById("input-passwd").value;
+    window.alert(LoginInfo.username + "+" + LoginInfo.password);
+}
