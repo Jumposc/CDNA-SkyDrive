e3cd5b49b4cc943faaff9d222acbc7cd061a337e
807477447 <807477447@qq.com>
2019-10-31 15:58:31 +0800

    添加Token验证接口

---
+++ CDNA-SkyDrive/API/TokenController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CDNA_SkyDrive.Control;
+using CDNA_SkyDrive.Mode;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CDNA_SkyDrive.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CheckToken()
+        {
+            if (Token.CheckToken(Request.Cookies["Token"]))
+                return Ok();
+            else
+                return BadRequest(JsonConvert.SerializeObject(new ReturnMode() { Data = "Token错误", Message = "Error" }));
+        }
+    }
+}
